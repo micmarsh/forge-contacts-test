c9539ff420032c2cec583c77c5dd9d8aa1180574
micmarsh <michael.marsh42@gmail.com>
2013-07-29 15:02:21 -0400

    some promise action going on

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -3,33 +3,49 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('contactlist', [ '$scope',
-    function($scope) {
+  controller('contactlist', [ '$scope', '$q',
+    function($scope, Q) {
         $scope.contacts = [{displayName: 'ass'}];
 
-        forge.contact.selectAll(
-            function (contacts) {
-                $scope.$apply(function(){
-                     $scope.contacts = contacts;
-                });
-            },
-            function (error) {
-                alert("you done fucked up");
-                alert(error);
-            }
-        )
+        // forge.contact.selectAll(
+        //     function (contacts) {
+        //         $scope.$apply(function(){
+        //              $scope.contacts = contacts;
+        //         });
+        //     },
+        //     function (error) {
+        //         alert("you done fucked up");
+        //         alert(error);
+        //     }
+        // )
+
+        var lulzPromise = function(){
+          var defered = Q.defer();
+          setTimeout(function(){
+            defered.resolve("poop");
+          },1);
+          return defered.promise;
+        }
+
+        var lulz = lulzPromise();
+        for(var i = 0; i < 30000; i++){
+          lulz.then(function (message) {
+             console.log(message);
+          })
+        }
+        
 
 
         $scope.clicked = function (contact) {
-           forge.contact.selectById(contact.id, function (fullContact) {
-                $scope.$apply( function () {
-                    $scope.textArea += fullContact.phoneNumbers[0].value;
-                });
-           }, function () {
+           // forge.contact.selectById(contact.id, function (fullContact) {
+           //      $scope.$apply( function () {
+           //          $scope.textArea += fullContact.phoneNumbers[0].value;
+           //      });
+           // }, function () {
 
-           });
+           // });
         }
-       // setInterval(function () {console.log($scope.textArea);}, 5000);
+       // setInterval(function () {console.log($scope.textArea);}, 5000); 
 
   // forge.contact.select(
   //       function (selected) {
