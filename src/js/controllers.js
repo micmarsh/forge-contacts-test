d133265529239394586952a5dec34c09353146ff
micmarsh <michael.marsh42@gmail.com>
2013-10-28 13:33:32 -0400

    dump a bunch of notes to main view

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -3,59 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('contactlist', [ '$scope', '$q', '$timeout',
-    function($scope, $q, $timeout) {
-        var FORGE = !!window.forge
-        var FAKES = [{name: "Me"},
-                        {name:"You"},
-                        {name: "Them"},
-                        {name: "name"},
-                        {name: "Nombre"},
-                        {name: "Hermano"},
-                        {name: "Homie"},
-                        {name: "Butts"}];
+  controller('notelist', [ '$scope', '$q', '$notes',
+    function($scope, $q, $notes) {
+      $notes.get().then(function(notes){
+        $scope.$apply(function(){
+          $scope.notes = notes;
+        })
+      });
 
-        function getMoreContacts (counter) {
-          var q = $q.defer();
-          if (FORGE)
-            forge.internal.call('get.some', {}, function (contacts) {
-              q.resolve(contacts);
-            }, function (error) {
-              q.reject(error);
-            });
-          else
-            $timeout(function () {
-              q.resolve(FAKES);
-            }, 500);
-          return q.promise; 
-        };
-        var counter = 0;
-
-        $scope.contacts = [];
-        $scope.loadMore = function () {
-           getMoreContacts(counter).then(function (contacts) {
-                //$scope.$apply(function () {
-                  for (var i = 0; i < contacts.length; i++) {
-                      $scope.contacts.push(contacts[i]);
-                  }
-                  counter += contacts.length; 
-               // });
-           
-          });
-        }
-        $scope.clicked = function (contact) {
-        
-        }
-
-        $scope.loadMore();
-       // setInterval(function () {console.log($scope.textArea);}, 5000); 
-
-  // forge.contact.select(
-  //       function (selected) {
-  //           alert(JSON.stringify(selected));
-  //       },
-  //       function () {}
-  //   );
   }])
   .controller('MyCtrl2', [function() {
 
--- src/js/services.js
+++ src/js/services.js
@@ -7,3 +7,39 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1');
+
+
+angular.module('myApp.services').service('$kinvey', [ function($kinvey){
+    var LULZ_APP = {
+        appKey: 'kid_ePfBz7N8xf',
+        appSecret: 'e13392c82e8b4ba597ad8c043c991100'
+    };
+
+    var REAL = {
+        appKey: 'kid_PVtSim6Wi5',
+        appSecret: 'c429fbc2a46d4ac4930f67ef7e4f8a8e'
+    };
+
+    var APP = REAL;
+    APP['sync'] = {
+        enable: true,
+        online: navigator.onLine
+    };
+
+    Kinvey.fnUserPromise = Kinvey.init(APP);
+    Kinvey.Sync.online();
+
+    return Kinvey;
+}]);
+
+angular.module('myApp.services').service('$notes', [ '$kinvey', function ($kinvey) {
+    this.get = function () {
+        var blankQuery = new Kinvey.Query()
+        var promise = Kinvey.DataStore.count('notes', blankQuery, {offline: true});
+        return promise.then(function(count){
+            var options = {};
+            options.offline = !!count
+            return Kinvey.DataStore.find('notes', blankQuery, options )
+        });
+    };
+}]);
